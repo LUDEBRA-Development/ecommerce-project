385ff0b5f75160a467d8cbb52ddf026ce1540a18
mibichoandafugao <augustohenao1234@gmail.com>
2025-04-03 21:54:34 -0500

    Header v1.1

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 export function Header() {
   return (
-    <div className='w-full h-14 bg-black flex justify-center items-center flex-row p-1'>
-      <ul className='flex flex-row justify-center items-center text-gray-300 gap-2.5 h-full font-bold'>
+    <div className='w-full h-20 bg-gray-50 flex justify-between items-center flex-row p-8'>
+      <Image src='/logo1.png' alt='Logo' width={80} height={80} />
+      <ul className='flex flex-row justify-center items-center text-gray-600 gap-2.5 h-full font-bold'>
         <li className='transition duration-300 ease-in-out p-1 rounded hover:text-black hover:bg-gray-300'>
           <Link href='/categories'>Productos</Link>
         </li>
