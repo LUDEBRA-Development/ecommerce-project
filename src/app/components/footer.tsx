d629dc054e92c93c390d3cc61c467037debed53f
mibichoandafugao <augustohenao1234@gmail.com>
2025-04-21 05:59:44 -0500

    Minor changes

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Footer() {
+  return (
+    <div className='flex flex-col bg-gray-800 w-full mt-5 border-[#6B056E] border-t-2'>
+      <div className='p-4 flex flex-row gap-6'>
+        <ul className=' flex flex-col gap-0.5'>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer mb-32 flex items-center justify-center'>
+            {/* aqui deberia ir el icono de la aplicacion */}
+            <Image src='/logo1.png' alt='Logo' width={80} height={80} />
+          </li>
+          <li className='text-gray-400 font-bold'>© 2025 Distr&Arte</li>
+        </ul>
+
+        <ul className=' flex flex-col gap-0.5 ml-12'>
+          <li className='text-white font-bold mb-2'>Resources</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>
+            <a
+              href='https://github.com/LUDEBRA-Development/ecommerce-project'
+              rel='noopener noreferrer'
+              target='_blank'
+            >
+              Repositorio
+            </a>
+          </li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>ajfkjdsf</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>adjfasdfjk</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>ajdfkjaskd</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>dsfsddsad</li>
+        </ul>
+
+        <ul className=' flex flex-col gap-0.5 ml-12'>
+          <li className='text-white font-bold mb-2'>About</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>
+            <Link href='/about-us'>Sobre Nosotros</Link>
+          </li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'> - </li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'> - </li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'> - </li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'> - </li>
+        </ul>
+
+        <ul className=' flex flex-col gap-0.5 ml-12'>
+          <li className='text-white font-bold mb-2'>Legal</li>
+          <li className='text-gray-400 font-bold hover:text-white cursor-pointer'>Privacy Policy</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- src/app/components/home-carrousel.tsx
+++ src/app/components/home-carrousel.tsx
@@ -4,57 +4,38 @@ import { Autoplay, Navigation } from 'swiper/modules'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import Image from 'next/image'
+import { Suspense } from 'react'
 
 export function HomeCarrousel() {
   return (
     <div className='flex flex-col w-full h-full'>
-      <h1 className='text-3xl text-black w-full pl-6 pt-2 font-medium'>
-        Ultimas Ofertas
-      </h1>
-      <Swiper
-        modules={[Autoplay, Navigation]}
-        spaceBetween={0}
-        slidesPerView={1}
-        autoplay={{
-          delay: 10000,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        className='mt-2.5 w-6xl h-3xl flex justify-center items-center rounded-3xl'
-      >
-        <SwiperSlide>
-          <Image
-            src='/prueba-imagen.webp'
-            alt='Banner'
-            width={1200}
-            height={400}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src='/prueba-imagen.webp'
-            alt='Banner'
-            width={1200}
-            height={400}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src='/prueba-imagen.webp'
-            alt='Banner'
-            width={1200}
-            height={400}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image
-            src='/prueba-imagen.webp'
-            alt='Banner'
-            width={1200}
-            height={400}
-          />
-        </SwiperSlide>
-      </Swiper>
+      <h1 className='text-3xl text-black w-full pl-6 pt-2 font-medium'>Ultimas Ofertas</h1>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Swiper
+          modules={[Autoplay, Navigation]}
+          spaceBetween={0}
+          slidesPerView={1}
+          autoplay={{
+            delay: 10000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          className='mt-2.5 w-6xl h-3xl flex justify-center items-center rounded-3xl'
+        >
+          <SwiperSlide>
+            <Image src='/prueba-imagen.webp' alt='Banner' width={1200} height={400} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src='/prueba-imagen.webp' alt='Banner' width={1200} height={400} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src='/prueba-imagen.webp' alt='Banner' width={1200} height={400} />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Image src='/prueba-imagen.webp' alt='Banner' width={1200} height={400} />
+          </SwiperSlide>
+        </Swiper>
+      </Suspense>
     </div>
   )
 }
